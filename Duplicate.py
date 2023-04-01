49f80b73ec5fbd16eff027b8e0df9a7050e2e249
Pavanms07 <59008808+Pavanms07@users.noreply.github.com>
2023-04-01 13:47:25 +0530

    Create Duplicate.py

---
+++ Duplicate.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+final=pd.DataFrame()
+# r"C:\Users\pavan\Downloads
+path = 'Downloads/Telegram Desktop/Pandas'
+for file in os.listdir(path):
+    if 'data2' in file:
+        p=path+'/'+file
+        print(p)
+        d=pd.read_csv(p)
+        df=pd.DataFrame(d)
+df1=df[['Registration Number','Student Name']].value_counts()
+df1
